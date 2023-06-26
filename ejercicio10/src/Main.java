2bf3033f01d3f136fd91e42c15e0e7222fc45225
Sosa Facundo <kiwiprojects.sosafacundon@gmail.com>
2023-06-26 20:10:48 -0300

    20:10

--- ejercicio10/src/Main.java
+++ ejercicio10/src/Main.java
@@ -1,3 +1,5 @@
+import java.text.DecimalFormat;
+import java.util.Arrays;
 import java.util.Random;
 
 public class Main {
@@ -5,45 +7,61 @@ public class Main {
         int arrayASize = 50;
         int arrayBSize = 20;
 
-        int[] arrayA = new int[arrayASize];
-        int[] arrayB = new int[arrayBSize];
+        float[] arrayA = new float[arrayASize];
+        float[] arrayB = new float[arrayBSize];
 
         fillInArray(arrayA, arrayASize);
         showArray(arrayA, arrayASize);
-
+        orderArray(arrayA, arrayASize, arrayB, arrayBSize);
 
     }
-
-    public static void fillInArray(int[] a, int aS) {
+    public static void fillInArray(float[] a, int aS) {
         Random r = new Random();
 
         for (int i = 0; i < aS; i++) {
-            //a[i] = r.nextInt(0, 99);
-            a[i] = i;
+            a[i] = r.nextFloat(0, 99);
         }
     }
-
-    public static void showArray(int[] a, int aS) {
+    public static void showArray(float[] a, int aS) {
         int breaker = 0;
 
+        DecimalFormat df = new DecimalFormat();
+        df.setMaximumFractionDigits(2);
+        System.out.println("");
+        System.out.println("");
         for (int i = 0; i < aS; i++) {
-            if (breaker == 9) {
+            if (breaker == 4) {
                 if (a[i] >= 0 && a[i] <= 9) {
-                    System.out.print(" |  " + a[i] + " |");
+                    System.out.print(" |  " + df.format(a[i]) + " |");
                 } else {
-                    System.out.print(" | " + a[i] + " |");
+                    System.out.print(" | " + df.format(a[i]) + " |");
                 }
                 System.out.println("");
                 breaker = 0;
             } else {
-                if (a[i] >= 0 && a[i] < 9) {
-                    System.out.print(" |  " + a[i]);
+                if (a[i] >= 0 && a[i] <= 9) {
+                    System.out.print(" |  " + df.format(a[i]));
                     breaker++;
                 } else {
-                    System.out.print(" | " + a[i]);
+                    System.out.print(" | " + df.format(a[i]));
                     breaker++;
                 }
             }
         }
+    }
+    public static void orderArray(float [] a, int aS, float[] b, int bS){
+
+
+        Arrays.sort(a);
+        showArray(a, aS);
+
+        for(int i = 0; i < bS; i++){
+            b[i] = a[i];
+            if (i > 9) {
+                b[i] = 0.50F;
+            }
+        }
+
+        showArray(b, bS);
     }
 }
